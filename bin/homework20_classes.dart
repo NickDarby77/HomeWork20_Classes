e8caf4f907749d80434bd540ee2ac61bd4edf72a
NickDarby77 <132682600+NickDarby77@users.noreply.github.com>
2023-07-10 17:40:32 +0600

    first commit

---
+++ bin/homework20_classes.dart
@@ -0,0 +1,95 @@
+import 'dart:math';
+
+// question 1
+abstract class Shape {
+  double calculateArea();
+}
+
+class Rectangle extends Shape {
+  double width;
+  double height;
+
+  Rectangle(this.width, this.height);
+
+  @override
+  double calculateArea() {
+    return width * height;
+  }
+}
+
+class Circle extends Shape {
+  double radius;
+
+  Circle(this.radius);
+
+  @override
+  double calculateArea() {
+    return pi * radius * radius;
+  }
+}
+
+// question 2
+abstract class Animal {
+  String name;
+  Animal(this.name);
+
+  void makeSound() {
+    print('make animal sound');
+  }
+}
+
+class Dog extends Animal {
+  Dog(super.name);
+
+  @override
+  void makeSound() {
+    print('gav gav');
+  }
+}
+
+class Cat extends Animal {
+  Cat(super.name);
+
+  @override
+  void makeSound() {
+    print('myau myau');
+  }
+}
+
+void main() {
+  // 1
+  List<Shape> shapes = [
+    Rectangle(4, 5),
+    Circle(3),
+    Rectangle(2, 8),
+    Circle(5),
+  ];
+
+  for (Shape element in shapes) {
+    double area = element.calculateArea();
+    print('The area of the shape is: $area');
+  }
+
+  // for (int i = 0; i < shapes.length; i++) {
+  //   Shape x = shapes[i];
+  //   double area = x.calculateArea();
+  //   print('The area of shape $i is: $area');
+  // }
+
+  // for (int i = 0; i < shapes.length; i++) {
+  //   print('The area of shape $i is: ${shapes[i].calculateArea()}');
+  // }
+
+  // 2
+  List<Animal> animals = [
+    Dog('Marta'),
+    Cat('Rex'),
+    Dog('Simba'),
+    Cat('Lisa'),
+  ];
+
+  for (Animal element in animals) {
+    // here we need FOR IN loop to call makesound(){} method
+    element.makeSound();
+  }
+}
---
+++ test/homework20_classes_test.dart
@@ -0,0 +1,8 @@
+import 'package:homework20_classes/homework20_classes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
